38f171a34afd17c0f9b72b390b393977eafb6953
Aiden Appleby <aiden.appleby3@gmail.com>
2022-05-31 10:03:50 -0700

    rough redirect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,20 @@ import type { AppProps } from 'next/app'
 import PageHead from '../components/PageHead'
 import { Children, useEffect } from 'react'
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 interface RouteGuardParams {
 	children: any;
 }
+
+const LookupRoute = async (path: string) => {
+  try {
+    const response = await axios.get(`https://api.hillview.tv/links/v1.1/check/${path}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+} 
 
 function HillviewWeb({ Component, pageProps }: AppProps) {
 
@@ -15,7 +25,15 @@ function HillviewWeb({ Component, pageProps }: AppProps) {
   const RouteProtection = (props: RouteGuardParams) => {
     const { children } = props;
 
-    useEffect(() => {
+    useEffect(async () => {
+      const response = await LookupRoute(router.asPath)
+      if (children.props.statusCode === 404 && router.asPath) {
+        console.log(response)
+        if (response.destination) {
+          router.replace(response.destination)
+          return
+        }
+      }
       if (children.props.redirect !== undefined) {
         router.push({
             pathname: children.props.redirect,
