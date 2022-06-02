416fcee0fa4da828b0418ddadb25b27cf28e7164
Aiden Appleby <aiden.appleby3@gmail.com>
2022-06-01 18:44:36 -0700

    better handling of wildcard routes

---
+++ pages/[wildcard].tsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import type { GetServerSideProps, NextPage } from 'next';
+
+const WildcardHandler: NextPage = () => {
+	return <></>;
+};
+
+const LookupRoute = async (path: string) => {
+	try {
+		const response = await axios.get(
+			`https://api.hillview.tv/links/v1.1/check/${path}`
+		);
+		return response.data;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const getServerSideProps = async (context: any) => {
+	const lookup = context.params.wildcard;
+	console.log(lookup);
+
+	try {
+		const response = await LookupRoute(lookup);
+
+		if (!response) {
+			return {
+				notFound: true,
+			};
+		}
+
+		if (response.destination) {
+			return {
+				redirect: {
+					destination: response.destination,
+					permanent: true,
+				},
+			};
+		} else {
+			return {
+				notFound: true,
+			};
+		}
+	} catch (error) {
+		return {
+			notFound: true,
+		};
+	}
+};
+
+export default WildcardHandler;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,74 +1,25 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import PageHead from "../components/PageHead";
-import { Children, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import axios from "axios";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import PageHead from '../components/PageHead';
+import { Children, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 interface RouteGuardParams {
-  children: any;
+	children: any;
 }
 
-const LookupRoute = async (path: string) => {
-  try {
-    const response = await axios.get(
-      `https://api.hillview.tv/links/v1.1/check/${path}`
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
 function HillviewWeb({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(true);
-  const [hasInitialized, setInitialization] = useState(false);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    init();
-  });
-
-  const init = async () => {
-    try {
-      if (hasInitialized) return true;
-
-      if (pageProps.statusCode === 404 && router.asPath) {
-
-        const response = await LookupRoute(router.asPath);
-        
-        if (!response) {
-          setLoading(false);
-          setInitialization(true);
-          return
-        }
-
-        if (response.destination) {
-          router.replace(response.destination);
-          return;
-        }
-      } else {
-        setLoading(false);
-        setInitialization(true);
-      }
-
-      return;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <>
-      <PageHead
-        title={pageProps.title}
-        description={pageProps.description}
-        image={pageProps.image}
-      />
-      {!loading ? <Component {...pageProps} /> : ""}
-    </>
-  );
+	return (
+		<>
+			<PageHead
+				title={pageProps.title}
+				description={pageProps.description}
+				image={pageProps.image}
+			/>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default HillviewWeb;
