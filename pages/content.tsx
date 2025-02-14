df50cbb3c5f2657d648c2d5b1cdadca37065a6dc
Aiden Appleby <aiden.appleby3@gmail.com>
2025-02-14 17:43:25 -0500

    highlighted content page

--- pages/content.tsx
+++ pages/content.tsx
@@ -22,12 +22,21 @@ export interface Video {
   inserted_at: Date;
 }
 
+export interface Rank {
+  rank: number;
+  video: Video;
+}
+
 interface ContentPageProps {
   videos: Video[];
+  highlightedVideos: Rank[];
 }
 
 const Content = (props: ContentPageProps) => {
   let [videos, setVideos] = useState(props.videos);
+  let [highlightedVideos, setHighlightedVideos] = useState(
+    props.highlightedVideos,
+  );
 
   const [searching, setSearching] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
@@ -86,17 +95,17 @@ const Content = (props: ContentPageProps) => {
 
   return (
     <Layout>
-      <div className="flex items-center justify-center w-full content-page h-fit">
-        <div className="w-11/12 h-fit max-w-screen-2xl">
+      <div className="content-page flex h-fit w-full items-center justify-center">
+        <div className="h-fit w-11/12 max-w-screen-2xl">
           {/* Header */}
-          <div className="header flex h-[275px] w-full items-center justify-center md:h-[450px]">
+          <div className="header mt-10 flex w-full items-center justify-center md:mt-14">
             <div className="center-object">
-              <h1 className="text-4xl font-semibold text-center sm:text-5xl md:text-6xl">
+              <h1 className="text-center text-4xl font-semibold sm:text-5xl md:text-6xl">
                 Our Content
               </h1>
               <input
                 type="text"
-                className="mb-10 mt-8 h-12 w-[320px] rounded-md bg-neutral-100 pl-5 outline outline-neutral-150 sm:w-[450px] md:mt-10 md:w-[550px]"
+                className="mt-8 h-12 w-[320px] rounded-md bg-neutral-100 pl-5 outline outline-neutral-150 sm:w-[450px] md:mt-10 md:w-[550px]"
                 placeholder="Search"
                 value={searchQuery}
                 onChange={(e) => {
@@ -106,10 +115,79 @@ const Content = (props: ContentPageProps) => {
             </div>
           </div>
 
-          {/* Video List */}
-          <div className="flex flex-wrap justify-around w-full gap-12 pb-10 video-list h-fit sm:gap-10">
+          {/* Highlighted List */}
+          <h1
+            className="py-16 text-center text-2xl font-semibold text-slate-600 sm:text-3xl md:text-4xl"
+            hidden={searchQuery.length > 0 || highlightedVideos == null}
+          >
+            Highlighted Content
+          </h1>
+          <div
+            className="video-list flex h-fit w-full flex-wrap justify-around gap-12 pb-10 sm:gap-10"
+            hidden={searchQuery.length > 0 || highlightedVideos == null}
+          >
+            <>
+              {searchQuery.length == 0 &&
+              highlightedVideos != null &&
+              highlightedVideos.length > 0
+                ? highlightedVideos.map((i) => {
+                    return (
+                      <Link href={"/watch?v=" + i.video.uuid} key={i.video.url}>
+                        <a>
+                          <div className="video group relative h-[180px] w-[320px] md:h-[275px] md:w-[460px]">
+                            <div className="video relative h-full w-full overflow-hidden">
+                              <div className="video-data absolute left-0 top-0 z-30 h-full w-full p-10 text-white opacity-0 duration-300 ease-in-out sm:group-hover:opacity-100">
+                                <h1 className="pb-3 text-3xl font-semibold">
+                                  {i.video.title}
+                                </h1>
+                                <p className="line-clamp-5">
+                                  {i.video.description}
+                                </p>
+                              </div>
+                              <div className="video-play opacity-1 absolute z-20 flex h-full w-full scale-100 items-center justify-center duration-200 ease-in-out sm:group-hover:scale-75 sm:group-hover:opacity-0">
+                                <svg
+                                  className="feather feather-play z-20 h-[70px] w-[70px] fill-white stroke-white opacity-80"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="24"
+                                  height="24"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                >
+                                  <polygon points="5 3 19 12 5 21 5 3"></polygon>
+                                </svg>
+                              </div>
+                              <div className="video-background absolute z-10 h-full w-full bg-black opacity-0 duration-300 ease-in-out sm:group-hover:opacity-40"></div>
+                              <div className="video-thumbnail relative h-full w-full duration-300 ease-in-out sm:group-hover:scale-110">
+                                <ContentImage
+                                  image={i.video.thumbnail}
+                                  alt={i.video.title}
+                                />
+                              </div>
+                            </div>
+                            <h1 className="text-md text-nowrap absolute bottom-[-30px] font-medium text-neutral-800 sm:hidden">
+                              {i.video.title}
+                            </h1>
+                          </div>
+                        </a>
+                      </Link>
+                    );
+                  })
+                : null}
+            </>
+          </div>
+          {/* All Videos */}
+          <h1
+            className="pb-14 pt-6 text-center text-2xl font-semibold text-slate-600 sm:text-3xl md:text-4xl"
+            hidden={searchQuery.length > 0 || highlightedVideos == null}
+          >
+            All Videos
+          </h1>
+          <div className="video-list flex h-fit w-full flex-wrap justify-around gap-12 pb-10 sm:gap-10">
             {searching ? (
-              <div className="relative flex items-center justify-center w-full">
+              <div className="relative flex w-full items-center justify-center">
                 <svg
                   version="1.1"
                   id="L9"
@@ -117,7 +195,7 @@ const Content = (props: ContentPageProps) => {
                   x="0px"
                   y="0px"
                   viewBox="0 0 100 100"
-                  className="w-20 h-20 fill-black stroke-black opacity-30"
+                  className="h-20 w-20 fill-black stroke-black opacity-30"
                 >
                   <path d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
                     <animateTransform
@@ -139,14 +217,14 @@ const Content = (props: ContentPageProps) => {
                     <Link href={"/watch?v=" + i.uuid} key={i.url}>
                       <a>
                         <div className="video group relative h-[180px] w-[320px] md:h-[315px] md:w-[560px]">
-                          <div className="relative w-full h-full overflow-hidden video">
-                            <div className="absolute top-0 left-0 z-30 w-full h-full p-10 text-white duration-300 ease-in-out opacity-0 video-data sm:group-hover:opacity-100">
+                          <div className="video relative h-full w-full overflow-hidden">
+                            <div className="video-data absolute left-0 top-0 z-30 h-full w-full p-10 text-white opacity-0 duration-300 ease-in-out sm:group-hover:opacity-100">
                               <h1 className="pb-5 text-4xl font-semibold">
                                 {i.title}
                               </h1>
                               <p className="line-clamp-5">{i.description}</p>
                             </div>
-                            <div className="absolute z-20 flex items-center justify-center w-full h-full duration-200 ease-in-out scale-100 video-play opacity-1 sm:group-hover:scale-75 sm:group-hover:opacity-0">
+                            <div className="video-play opacity-1 absolute z-20 flex h-full w-full scale-100 items-center justify-center duration-200 ease-in-out sm:group-hover:scale-75 sm:group-hover:opacity-0">
                               <svg
                                 className="feather feather-play z-20 h-[70px] w-[70px] fill-white stroke-white opacity-80"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -161,12 +239,12 @@ const Content = (props: ContentPageProps) => {
                                 <polygon points="5 3 19 12 5 21 5 3"></polygon>
                               </svg>
                             </div>
-                            <div className="absolute z-10 w-full h-full duration-300 ease-in-out bg-black opacity-0 video-background sm:group-hover:opacity-40"></div>
-                            <div className="relative w-full h-full duration-300 ease-in-out video-thumbnail sm:group-hover:scale-110">
+                            <div className="video-background absolute z-10 h-full w-full bg-black opacity-0 duration-300 ease-in-out sm:group-hover:opacity-40"></div>
+                            <div className="video-thumbnail relative h-full w-full duration-300 ease-in-out sm:group-hover:scale-110">
                               <ContentImage image={i.thumbnail} alt={i.title} />
                             </div>
                           </div>
-                          <h1 className="text-md absolute bottom-[-30px] font-medium text-neutral-800 sm:hidden">
+                          <h1 className="text-md text-nowrap absolute bottom-[-30px] font-medium text-neutral-800 sm:hidden">
                             {i.title}
                           </h1>
                         </div>
@@ -216,10 +294,26 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
 
     const data = await response.json();
 
+    const response2 = await fetch(
+      "https://api.hillview.tv/video/v1.1/spotlight?limit=24&offset=0&search=",
+    );
+
+    if (!response2.ok) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    const data2 = await response2.json();
+
     return {
       props: {
         title: "HillviewTV - Content",
         videos: data,
+        highlightedVideos: data2.data,
       },
     };
   } catch (error) {
