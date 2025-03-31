1f1b5d3800525034a6ad48784854ba464302b0a2
Aiden Appleby <aiden.appleby3@gmail.com>
2025-03-31 16:00:20 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    unoptomized: true,
+    unoptimized: true,
     domains: [
       "content.hillview.tv",
       "customer-nakrsdfbtn3mdz5z.cloudflarestream.com",
